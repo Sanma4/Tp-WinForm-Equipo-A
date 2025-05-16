935bff22badca7c034fd3dad7ae6ba586e8b5631
sanma4 <sanmartinobenjamin@gmail.com>
2025-05-16 19:50:41 +0200

    Agregado todos los metodos de acceso datos

---
+++ AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector { get; }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server= .\\SQLEXPRESS; Database=Catalogo_P3_DB; integrated security = true;");
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor); 
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+               lector.Close();
+            conexion.Close();
+        }
+    }
+}
